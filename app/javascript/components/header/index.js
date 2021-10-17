4764541b80e6c6c3026c4025eea107cb958f5b49
UsmanFayyaz87 <fayyaz@sozie.com>
2021-10-17 21:57:05 +0500

    header working statrted

---
+++ app/javascript/components/header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import Logo from "../../../assets/images/logo.jpg";
+
+const Header = () => {
+  return (
+    <header id="page-topbar">
+      {/* <div>
+        <Link to="#">
+          <img src={Logo} />
+        </Link>
+      </div>
+      <div></div> */}
+    </header>
+  );
+};
+
+export default Header;
