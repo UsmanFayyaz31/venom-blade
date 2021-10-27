60635cc58960aa6b05662414590d1da5656a9da0
UsmanFayyaz87 <fayyaz@sozie.com>
2021-10-27 23:53:01 +0500

    images flow complete for products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,43 +1,48 @@
 class Api::V1::ProductsController < ApplicationController
-    def index
-        @products = Product.all
-        render json: @products
+  def index
+    @products = Product.all.with_attached_images
+    render json: @products.map {|product| product.as_json.merge({product_images: product.images.map {|img| url_for(img)} })}
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render json: @product
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product
+    else
+      render error: { error: 'Unable to create product'}, status: 400
     end
+  end
 
-    def show
-        @product = Product.find(params[:id])
-        render json: @product
+  def update
+    @product = Product.find(params[:id])
+
+    if @product
+      product.update(product_params)
+      render json:{message: 'Product created successfully'}, status:200
+    else
+      render error: { error: 'Unable to update product'}, status: 400
     end
+  end
 
-    def create
-        @product = Product.new(product_params)
+  def destroy
+    @product = Pronduct.find(params[:id])
 
-        if @product.save
-            render json: @product
-        else
-            render error: { error: 'Unable to create product'}, status: 400
-        end
+    if @product
+      @product.destroy
+      render json: {message:'Product deleted successfully.'},status: 200
+    else
+      render error: { error: 'Unable to delete product'}, status: 400
     end
+  end
 
-    def update
-      @product = Product.find(params[:id])
-
-      if @product
-        product.update(product_params)
-        render json:{message: 'Product created successfully'}, status:200
-      else
-        render error: { error: 'Unable to update product'}, status: 400
-      end
-    end
-
-    def destroy
-      @product = Pronduct.find(params[:id])
-
-      if @product
-        @product.destroy
-        render json: {message:'Product deleted successfully.'},status: 200
-      else
-        render error: { error: 'Unable to delete product'}, status: 400
-      end
-    end
+  private
+  def product_params
+    params.require(:product).permit(images: [])
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ApplicationRecord
     belongs_to :category
+    has_many_attached :images
+
+    def display_name
+        product_name
+    end
 end
