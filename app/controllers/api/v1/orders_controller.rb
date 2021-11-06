8ae1dcc765e5a59e3c8a198992af1d795f031f93
UsmanFayyaz87 <fayyaz@sozie.com>
2021-11-06 18:42:28 +0500

    flow complete

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -11,12 +11,6 @@ class Api::V1::OrdersController < ApplicationController
 
   def create
     @orders = Order.new(order_params)
-    remaining = 10 - (Date.today - order_params["ordered_date"].to_date).to_i
-    if remaining <= 0
-      @orders.remaining_days = 0
-    else
-      @orders.remaining_days = remaining
-    end
 
     if @orders.save
       render json: @orders
