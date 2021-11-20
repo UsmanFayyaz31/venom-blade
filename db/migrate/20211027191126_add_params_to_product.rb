35efd08af0c482311bfb2f2c4674520ddac7957e
usmanfayyaz <fayyaz@sozie.com>
2021-11-20 15:09:47 +0500

    correcting migration

--- db/migrate/20211027191126_add_params_to_product.rb
+++ db/migrate/20211027191126_add_params_to_product.rb
@@ -1,8 +1,8 @@
 class AddParamsToProduct < ActiveRecord::Migration[6.1]
   def change
-    add_column :products, :full_length, :integar
-    add_column :products, :blade_length, :integar
-    add_column :products, :handle_length, :integar
+    add_column :products, :full_length, :integer
+    add_column :products, :blade_length, :integer
+    add_column :products, :handle_length, :integer
     add_column :products, :material, :string
     add_column :products, :sheath, :boolean
   end
--- db/migrate/20211106084823_add_params_to_order.rb
+++ db/migrate/20211106084823_add_params_to_order.rb
@@ -1,7 +1,7 @@
 class AddParamsToOrder < ActiveRecord::Migration[6.1]
   def change
-    add_column :orders, :remaining_days, :integar
+    add_column :orders, :remaining_days, :integer
     add_column :orders, :delivered, :boolean
-    add_column :orders, :quantity, :integar
+    add_column :orders, :quantity, :integer
   end
 end
