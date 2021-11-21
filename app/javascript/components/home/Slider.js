c659717f9366831a3db5e93452b7df6fb8fa3e9e
usmanfayyaz <fayyaz@sozie.com>
2021-11-21 23:44:34 +0500

    slider issue resolved

--- app/javascript/components/home/Slider.js
+++ app/javascript/components/home/Slider.js
@@ -1,7 +1,6 @@
 import React from "react";
 import AwesomeSlider from "react-awesome-slider";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
-import "../../../../node_modules/react-awesome-slider/dist/styles.css";
 
 const AutoplaySlider = withAutoplay(AwesomeSlider);
 
